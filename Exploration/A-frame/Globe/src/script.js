200a7db12c36f910a44e8d303c353e9efa025a37
ze-antunes <jprantunes2000@gmail.com>
2024-04-19 00:21:18 +0100

    CHORE[EXP]: Explore Aframe and Threejs UI components
    Explore the different components from Aframe and Threejs to see if there's any good UI to use a base to the project menus
    
    This commit is associated with the issue https://app.clickup.com/t/86bydqujv

--- Exploration/A-frame/Globe/src/script.js
+++ Exploration/A-frame/Globe/src/script.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import GUI from 'lil-gui';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { CSG } from 'three-csg-ts';
+import ThreeMeshUI from 'three-mesh-ui'
 
 // Import Shaders 
 import earthVertexShader from './shaders/earth/vertex.glsl'
@@ -27,10 +28,10 @@ let leftHand = document.getElementById("leftController")
 AFRAME.registerComponent('thumbstick-logging', {
     init: function () {
         this.el.addEventListener('thumbstickmoved', () => {
-            
+
         });
         this.el.addEventListener('triggerdown', () => {
-            
+
         });
         this.el.addEventListener('abuttondown', () => {
             if (toggleState) {
@@ -725,8 +726,425 @@ let tick = () => {
 
     earth.rotation.y = elapsedTime * 0.05
 
+    // Don't forget, ThreeMeshUI must be updated manually.
+    // This has been introduced in version 3.0.0 in order
+    // to improve performance
+    ThreeMeshUI.update();
+
+    meshContainer.rotation.z += 0.01;
+    meshContainer.rotation.y += 0.01;
+
+    updateButtons();
+
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
+// tick()
+
+
+// function makeTextPanel() {
+
+//     const container = new ThreeMeshUI.Block({
+//         width: 1.2,
+//         height: 0.5,
+//         padding: 0.05,
+//         justifyContent: 'center',
+//         alignContent: 'left',
+//         fontFamily: 'https://unpkg.com/three-mesh-ui/examples/assets/Roboto-msdf.json',
+//         fontTexture: 'https://unpkg.com/three-mesh-ui/examples/assets/Roboto-msdf.png'
+//     });
+
+//     container.position.set(0, 0, 0);
+//     container.rotation.x = -0.3;
+//     scene.add(container);
+
+//     //
+
+//     container.add(
+
+//         new ThreeMeshUI.Text({
+//             content: "Ze",
+//             fontSize: 0.08
+//         }),
+//     );
+
+// }
+
+// makeTextPanel();
+
+import FontJSON from './assets/Roboto-msdf.json';
+import FontImage from './assets/Roboto-msdf.png';
+
+let meshContainer, meshes, currentMesh;
+const objsToTest = [];
+
+// compute mouse position in normalized device coordinates
+// (-1 to +1) for both directions.
+// Used to raycasting against the interactive elements
+
+const raycaster = new THREE.Raycaster();
+
+const mouse = new THREE.Vector2();
+mouse.x = mouse.y = null;
+
+let selectState = false;
+
+window.addEventListener('pointermove', (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+
+window.addEventListener('pointerdown', () => {
+    selectState = true;
+});
+
+window.addEventListener('pointerup', () => {
+    selectState = false;
+});
+
+window.addEventListener('touchstart', (event) => {
+    selectState = true;
+    mouse.x = (event.touches[0].clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.touches[0].clientY / window.innerHeight) * 2 + 1;
+});
+
+window.addEventListener('touchend', () => {
+    selectState = false;
+    mouse.x = null;
+    mouse.y = null;
+});
+
+//
+
+////////////////////
+// Primitive Meshes
+////////////////////
+
+meshContainer = new THREE.Group();
+meshContainer.position.set(0, 1, -1.9);
+scene.add(meshContainer);
+
+const sphere = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(0.3, 1),
+    new THREE.MeshStandardMaterial({ color: 0x3de364, flatShading: true })
+);
+
+const box = new THREE.Mesh(
+    new THREE.BoxGeometry(0.45, 0.45, 0.45),
+    new THREE.MeshStandardMaterial({ color: 0x643de3, flatShading: true })
+);
+
+const cone = new THREE.Mesh(
+    new THREE.ConeGeometry(0.28, 0.5, 10),
+    new THREE.MeshStandardMaterial({ color: 0xe33d4e, flatShading: true })
+);
+
+//
+
+sphere.visible = box.visible = cone.visible = false;
+
+meshContainer.position.set(0, 1.5, 1);
+meshContainer.add(sphere, box, cone);
+
+meshes = [sphere, box, cone];
+currentMesh = 0;
+
+showMesh(currentMesh);
+
+//////////
+// Panel
+//////////
+
+makePanel();
+
+// Shows the primitive mesh with the passed ID and hide the others
+
+function showMesh(id) {
+
+    meshes.forEach((mesh, i) => {
+
+        mesh.visible = i === id ? true : false;
+
+    });
+
+}
+
+///////////////////
+// UI contruction
+///////////////////
+
+function makePanel() {
+
+    // Container block, in which we put the two buttons.
+    // We don't define width and height, it will be set automatically from the children's dimensions
+    // Note that we set contentDirection: "row-reverse", in order to orient the buttons horizontally
+
+    const container = new ThreeMeshUI.Block({
+        justifyContent: 'center',
+        contentDirection: 'row-reverse',
+        fontFamily: FontJSON,
+        fontTexture: FontImage,
+        fontSize: 0.07,
+        padding: 0.02,
+        borderRadius: 0.11
+    });
+
+    container.position.set(0, 0.6, 1.2);
+    container.rotation.x = -0.55;
+    scene.add(container);
+
+    // BUTTONS
+
+    // We start by creating objects containing options that we will use with the two buttons,
+    // in order to write less code.
+
+    const buttonOptions = {
+        width: 0.4,
+        height: 0.15,
+        justifyContent: 'center',
+        offset: 0.05,
+        margin: 0.02,
+        borderRadius: 0.075
+    };
+
+    // Options for component.setupState().
+    // It must contain a 'state' parameter, which you will refer to with component.setState( 'name-of-the-state' ).
+
+    const hoveredStateAttributes = {
+        state: 'hovered',
+        attributes: {
+            offset: 0.035,
+            backgroundColor: new THREE.Color(0x999999),
+            backgroundOpacity: 1,
+            fontColor: new THREE.Color(0xffffff)
+        },
+    };
+
+    const idleStateAttributes = {
+        state: 'idle',
+        attributes: {
+            offset: 0.035,
+            backgroundColor: new THREE.Color(0x666666),
+            backgroundOpacity: 0.3,
+            fontColor: new THREE.Color(0xffffff)
+        },
+    };
+
+    // Buttons creation, with the options objects passed in parameters.
+
+    const buttonNext = new ThreeMeshUI.Block(buttonOptions);
+    const buttonPrevious = new ThreeMeshUI.Block(buttonOptions);
+
+    // Add text to buttons
+
+    buttonNext.add(
+        new ThreeMeshUI.Text({ content: 'next' })
+    );
+
+    buttonPrevious.add(
+        new ThreeMeshUI.Text({ content: 'previous' })
+    );
+
+    // Create states for the buttons.
+    // In the loop, we will call component.setState( 'state-name' ) when mouse hover or click
+
+    const selectedAttributes = {
+        offset: 0.02,
+        backgroundColor: new THREE.Color(0x777777),
+        fontColor: new THREE.Color(0x222222)
+    };
+
+    buttonNext.setupState({
+        state: 'selected',
+        attributes: selectedAttributes,
+        onSet: () => {
+
+            currentMesh = (currentMesh + 1) % 3;
+            showMesh(currentMesh);
+
+        }
+    });
+    buttonNext.setupState(hoveredStateAttributes);
+    buttonNext.setupState(idleStateAttributes);
+
+    //
+
+    buttonPrevious.setupState({
+        state: 'selected',
+        attributes: selectedAttributes,
+        onSet: () => {
+
+            currentMesh -= 1;
+            if (currentMesh < 0) currentMesh = 2;
+            showMesh(currentMesh);
+
+        }
+    });
+    buttonPrevious.setupState(hoveredStateAttributes);
+    buttonPrevious.setupState(idleStateAttributes);
+
+    buttonNext.mixin = 'box'
+    buttonNext['intersect-color-change'] = ''
+    // buttonPrevious.setAttribute('mixin', 'box');
+
+    console.log(buttonNext)
+    console.log(document.getElementById("box"))
+
+    //
+
+    container.add(buttonNext, buttonPrevious);
+    objsToTest.push(buttonNext, buttonPrevious);
+
+}
+
+// Called in the loop, get intersection with either the mouse or the VR controllers,
+// then update the buttons states according to result
+
+function updateButtons() {
+
+    // Find closest intersecting object
+
+    let intersect;
+
+    if (renderer != undefined && renderer.xr.isPresenting) {
+
+        vrControl.setFromController(0, raycaster.ray);
+
+        intersect = raycast();
+
+        // Position the little white dot at the end of the controller pointing ray
+        if (intersect) vrControl.setPointerAt(0, intersect.point);
+
+    } else if (mouse.x !== null && mouse.y !== null) {
+
+        raycaster.setFromCamera(mouse, camera);
+
+        intersect = raycast();
+
+    }
+
+    // Update targeted button state (if any)
+
+    if (intersect && intersect.object.isUI) {
+
+        if (selectState) {
+
+            // Component.setState internally call component.set with the options you defined in component.setupState
+            intersect.object.setState('selected');
+
+        } else {
+
+            // Component.setState internally call component.set with the options you defined in component.setupState
+            intersect.object.setState('hovered');
+
+        }
+
+    }
+
+    // Update non-targeted buttons state
+
+    objsToTest.forEach((obj) => {
+
+        if ((!intersect || obj !== intersect.object) && obj.isUI) {
+
+            // Component.setState internally call component.set with the options you defined in component.setupState
+            obj.setState('idle');
+
+        }
+
+    });
+
+}
+
+//
+
+function raycast() {
+
+    return objsToTest.reduce((closestIntersection, obj) => {
+
+        const intersection = raycaster.intersectObject(obj, true);
+
+        if (!intersection[0]) return closestIntersection;
+
+        if (!closestIntersection || intersection[0].distance < closestIntersection.distance) {
+
+            intersection[0].object = obj;
+
+            return intersection[0];
+
+        }
+
+        return closestIntersection;
+
+    }, null);
+
+}
+
+
 tick()
+
+
+AFRAME.registerComponent('boxes', {
+    init: function () {
+        var box;
+        var columns = 20;
+        var el = this.el;
+        var i;
+        var j;
+        var rows = 15;
+
+        if (el.sceneEl.isMobile) {
+            columns = 10;
+            rows = 5;
+        };
+
+        for (let x = columns / -2; x < columns / 2; x++) {
+            for (let y = 0.5; y < rows; y++) {
+                box = document.createElement('a-entity');
+                box.setAttribute('mixin', 'box');
+                box.setAttribute('position', { x: x * .6, y: y * .6, z: 1.5 });
+                el.appendChild(box);
+            }
+        }
+    }
+});
+
+AFRAME.registerComponent('shadow-if-mobile', {
+    init: function () {
+        if (!this.el.sceneEl.isMobile) {
+            this.el.setAttribute('light', {
+                castShadow: true,
+                shadowMapWidth: 2048,
+                shadowMapHeight: 1024
+            });
+        }
+    }
+});
+
+AFRAME.registerComponent('intersectColorChange', {
+    init: function () {
+        var el = this.el;
+        var material = el.getAttribute('material');
+        var initialColor = material.color;
+        var self = this;
+
+        el.addEventListener('mousedown', function (evt) {
+            el.setAttribute('material', 'color', '#EF2D5E');
+        });
+
+        el.addEventListener('mouseup', function (evt) {
+            el.setAttribute('material', 'color', self.isMouseEnter ? '#24CAFF' : initialColor);
+        });
+
+        el.addEventListener('mouseenter', function () {
+            el.setAttribute('material', 'color', '#24CAFF');
+            self.isMouseEnter = true;
+        });
+
+        el.addEventListener('mouseleave', function () {
+            el.setAttribute('material', 'color', initialColor);
+            self.isMouseEnter = false;
+        });
+    }
+});
